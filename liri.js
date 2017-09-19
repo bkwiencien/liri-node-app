9e05b75cf3919d717a5a6a48b6df117a012cbdef
Bob Kwiencien <bkwiencien@gmail.com>
2017-09-19 17:13:16 -0400

    added skeletal functions

--- liri.js
+++ liri.js
@@ -8,3 +8,28 @@ twitterAppName = "liri-node-app-bk";
 defaultMovie   = "Mr. Nobody";
 defaultSong    = "The Sign";
 console.log(keys);
+switch (process.argv[2]) {
+    case 'my-tweets':
+      getMyTweets();
+      break;
+    case 'spotify-this-song':
+      getMeSpotifyStuff(process.argv[3]);
+      break;
+    case 'movie-this':
+      getMovieStuff(process.argv[3]);
+      break;
+    case 'do-what-it-says':
+     // doWhatItSays();
+      break;
+    default:
+      console.log("I cannot figure out what you want!");
+  }
+  function getMyTweets() {
+  	console.log("in getMyTweets");
+  }
+  function getSpotifyStuff(songName){
+  	console.log("in getSpotifyStuff");
+  }
+  function getMovieStuff(maveNAme) {
+  	console.log("in getMovieStuff");
+  }
