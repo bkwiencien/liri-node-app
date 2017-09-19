4ab61171f4f22831df23e7dd0fdd2e098ecc3e29
Bob Kwiencien <bkwiencien@gmail.com>
2017-09-19 16:25:48 -0400

    spotify keys

--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
 var keys = require('./keys');
 var omdbApiKey = "40e9cece";
+spotifyClientID = "d30b40453f4b4c91891565a41bb149e5";
+spotifyClientSecret = "8f425f8bee264d22ac9aa25b7d05d113";
 console.log(keys);
