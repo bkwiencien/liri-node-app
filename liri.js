dd7f7bccf91ec531417a8b2441c93c253b7f92c7
Bob Kwiencien <bkwiencien@gmail.com>
2017-09-19 17:04:20 -0400

    added fs

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 var keys = require('./keys');
+var fs   = require('fs');
 var omdbApiKey = "40e9cece";
 spotifyClientID = "d30b40453f4b4c91891565a41bb149e5";
 spotifyClientSecret = "8f425f8bee264d22ac9aa25b7d05d113";
