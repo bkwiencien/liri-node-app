ba30030bebb3dfd27e537eeeca9e402d973b1d66
Bob Kwiencien <bkwiencien@gmail.com>
2017-09-19 14:01:12 -0400

    added keys

--- keys.js
+++ keys.js
@@ -1,10 +1,11 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'WnFmt9Sn70CKJjHOh75ANoksr',
+  consumer_secret: 'p7R6jAii2C3xaEmafm3GAAkMAjTsv11I3o34p3h5aoj4V3Dohz',
+  access_token_key: '53948968-NuBZnqaB2h0N4Q5U4680IKnCV6MyQqJRMnhjBlSoz',
+  access_token_secret: 'xmyknGLFCCl0Ql6gCXCyj2RtvJ0ufuZmA71AKHLYUtNPE',
 }
 
 module.exports = twitterKeys;
+// liri-node-app-bk
