4344c85ddb08edb74de4cc63f2586c5e52715b97
Bob Kwiencien <bkwiencien@gmail.com>
2017-09-20 08:35:58 -0400

    twitter

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ var keys = require('./keys');
 var fs   = require('fs');
 var request = require('request');
 var Spotify = require('spotify');
+var twitter = require('twitter');
 var omdbApiKey = "40e9cece";
 var spotifyClientID = "d30b40453f4b4c91891565a41bb149e5";
 var spotifyClientSecret = "8f425f8bee264d22ac9aa25b7d05d113";
