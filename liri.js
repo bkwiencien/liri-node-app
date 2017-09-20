c18d666c13a4befe57403cfb1e3b98f872fb6887
Bob Kwiencien <bkwiencien@gmail.com>
2017-09-20 05:34:44 -0400

    added spotify

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 var keys = require('./keys');
 var fs   = require('fs');
 var request = require('request');
+var spotify = require('spotify');
 var omdbApiKey = "40e9cece";
 spotifyClientID = "d30b40453f4b4c91891565a41bb149e5";
 spotifyClientSecret = "8f425f8bee264d22ac9aa25b7d05d113";
